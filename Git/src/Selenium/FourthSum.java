cdd0711d798531ccb8d16f9ec51563fb80ab20e4
akhilr <akhilr@VMT12900.VALUEMOMENTUM.BIZ>
2023-01-20 10:12:57 +0530

    Fourth Commit

---
+++ Git/src/Selenium/FourthSum.java
@@ -0,0 +1,23 @@
+package Selenium;
+
+public class FourthSum {
+
+	public static void main(String[] args) {
+		  
+		int arr[]= {1,2,3,4,5};
+		int temp;
+		for(int i=0;i<arr.length;i++) {
+		     for(int j=i;j<arr.length;j++) {
+		         if(arr[i]<arr[j]) {
+		             temp=arr[i];
+		             arr[i]=arr[j];
+		             arr[j]=temp; 
+		         }
+		     }
+		}
+		for(int k:arr)
+		     System.out.println(k);
+
+	}
+
+}
